569cfed986a8dcfc6dd008e7d18cbeb58b5d5ca1
SvetlanaPopova <sv_popova@bk.ru>
2015-05-26 06:28:27 +0300

    В методе edit_first_conact локатор кнопки выделен в отдельный параметр

--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -2,9 +2,21 @@ __author__ = 'User'
 from model.contact import Contact
 
 
-def test_edit_first_contact(app):
+def test_edit_first_contact_top_button_update(app):
     app.session.login(username="admin", password="secret")
     app.contact.edit_first_contact(Contact(firstname="NewName", lastname="NewLastName", nickname="NewContact1",
-                        title="NewTitle", company="NewCompany", address="-", telhome="1234", telmobile="4567",
-                        telwork="7890", telfax="1234567890", homepage="contactnew.homepage"))
+                                           title="NewTitle", company="NewCompany", address="-", telhome="1234",
+                                           telmobile="4567",
+                                           telwork="7890", telfax="1234567890", homepage="contactnew.homepage"),
+                                   locator_button_update="//*[@id='content']/form[1]/input[1]")
+    app.session.logout()
+
+
+def test_edit_first_contact_bottom_button_update(app):
+    app.session.login(username="admin", password="secret")
+    app.contact.edit_first_contact(Contact(firstname="NewName", lastname="NewLastName", nickname="NewContact1",
+                                           title="NewTitle", company="NewCompany", address="-", telhome="1234",
+                                           telmobile="4567",
+                                           telwork="7890", telfax="1234567890", homepage="contactnew.homepage"),
+                                   locator_button_update="//*[@value='Update'][2]")
     app.session.logout()
