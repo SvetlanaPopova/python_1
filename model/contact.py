5908f402a4fcaa621bc4746617d68dfbc481564b
SvetlanaPopova <sv_popova@bk.ru>
2015-06-23 23:22:23 +0300

    Параметризованы тесты для создания групп и контактов

--- model/contact.py
+++ model/contact.py
@@ -28,15 +28,14 @@ class Contact:
         self.all_emails_from_home_page = all_emails_from_home_page
 
     def __repr__(self):
-        return "%s:%s:%s" % (self.id, self.firstname, self.lastname)
+        return "%s:%s:%s:%s:%s:%s:%s:%s:%s" % (self.id, self.firstname, self.lastname, self.company, self.address,
+                                               self.homephone, self.mobilephone, self.workphone, self.email_1)
 
     def __eq__(self, other):
         return (self.id is None or other.id is None or self.id == other.id)\
                and (self.firstname == other.firstname)\
                and (self.lastname == other.lastname)\
-               and (self.address == other.address)\
-               #and (self.all_phones_from_home_page == other.all_phones_from_home_page)\
-               #and (self.all_emails_from_home_page == other.all_emails_from_home_page)
+               and (self.address == other.address)
 
     def id_or_max(self):
         if self.id:
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,12 +1,32 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
+import pytest
+import random
+import string
 
 
-def test_add_contact(app):
+def random_string (prefix, maxlen):
+    symbols = string.ascii_letters*3 + string.digits + " "*10 #+ string.punctuation
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+def random_string_for_phones (prefix, maxlen):
+    symbols = string.ascii_letters + string.digits*15 + " "*10 #+ string.punctuation
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+testdata = [Contact(firstname="", lastname="", company="", address="", homephone="", mobilephone="",
+                        workphone="", email_1="")] + [
+    Contact(firstname=random_string("firstname", 10), lastname=random_string("lastname", 10),
+            company=random_string("company", 10), address=random_string("address", 30),
+            homephone=random_string_for_phones("homephone", 20), mobilephone=random_string_for_phones("mobilephone", 20),
+            workphone=random_string_for_phones("workphone", 20), email_1=random_string("email", 15))
+    for i in range(5)
+]
+
+
+@pytest.mark.parametrize ("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(firstname="ContactName", lastname="ContactLastName", nickname="Contact1",
-                        title="ContactTitle", company="NewCompany", address="Moscow", homephone="123", mobilephone="456",
-                        workphone="789", fax="123456789", homepage="contact.homepage", secondaryphone="secondary123")
     app.contact.add_new(contact)
     app.contact.check_add_new_success(contact, old_contacts)
 
